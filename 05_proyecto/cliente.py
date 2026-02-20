f6a0e29502b073ee5170c16290f5b1d462b51398
jonh e muñoz <jemuo@sitio.com>
2026-02-20 17:33:29 -0500

    final

--- 05_proyecto/cliente.py
+++ 05_proyecto/cliente.py
@@ -1,2 +1,34 @@
 # Cliente
 
+import socket
+import threading
+
+def recibir_mensajes(sock):
+    while True:
+        try:
+            mensaje = sock.recv(1024).decode('utf-8')
+            print(f"\n{mensaje}")
+        except:
+            print("[!] La conexion ya esta cerrada.")
+            break
+
+def iniciar_cliente():
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect(('127.0.0.1', 5000))
+
+    nombre = input("Ingresa tu nombre por favor: ")
+
+
+    hilo_recibo = threading.Thread(target=recibir_mensajes, args=(cliente,))
+    hilo_recibo.daemon = True
+    hilo_recibo.start()
+
+    while True:
+        msg = input("> ")
+        if msg.lower() == 'salir': break
+        cliente.send(f"{nombre}: {msg}".encode('utf-8'))
+
+    cliente.close()
+
+if __name__ == "__main__":
+    iniciar_cliente()
--- 05_proyecto/servidor.py
+++ 05_proyecto/servidor.py
@@ -1,2 +1,49 @@
 # Servidor
 
+import socket
+import threading
+
+
+HOST = '127.0.0.1'  
+PORT = 5000        
+clientes = []
+
+def broadcast(mensaje, _cliente_actual):
+    """Envía el mensaje a todos los clientes excepto al que lo envió."""
+    for cliente in clientes:
+        if cliente != _cliente_actual:
+            try:
+                cliente.send(mensaje)
+            except:
+                cliente.close()
+                clientes.remove(cliente)
+
+def manejar_cliente(cliente):
+    """Gestiona la conexión individual de cada cliente."""
+    while True:
+        try:
+            mensaje = cliente.recv(1024)
+            if not mensaje: break
+            broadcast(mensaje, cliente)
+        except:
+            clientes.remove(cliente)
+            cliente.close()
+            break
+
+def iniciar_servidor():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
+    server.bind((HOST, PORT))
+    server.listen()
+    print(f"[*] Servidor escuchando {HOST}:{PORT}")
+
+    while True:
+        cliente_socket, direccion = server.accept()
+        print(f"[*] Nueva conexión iniciada desde {direccion}")
+        clientes.append(cliente_socket)
+        
+       
+        hilo = threading.Thread(target=manejar_cliente, args=(cliente_socket,))
+        hilo.start()
+
+if __name__ == "__main__":
+    iniciar_servidor()
